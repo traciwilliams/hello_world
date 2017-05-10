93c689a0c0d3695223ec3fe0459fd8171d1f5d23
Traci Williams <ug3000@gmail.com>
2017-05-09 19:41:32 -0500

    divs

---
+++ hello_world/src/App.js
@@ -0,0 +1,19 @@
+// bring in React and Component from React
+//this imports react methods and the component class from the react library
+import React, {Component} from 'react';
+
+// define our Hello component that we are creating
+class Hello extends Component {
+  // what should the component render?
+  render () {
+    // make sure to return some UI
+    return (
+    <div>	
+      <h1>Hello World!</h1>
+      <h3>It's time for tea!</h3>
+    </div>  
+    )
+  }
+}
+
+export default Hello
